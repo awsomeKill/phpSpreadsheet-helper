a91bf4e08b483e9ecd90a7270e0148e77440cf9c
xuwen <xuwen@weiwoju.com>
2019-05-17 11:26:07 +0800

    test

---
+++ src/phpSpreadsheet/Helper.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/5/17
+ * Time: 10:54
+ */
+
+namespace phpSpreadsheet;
+
+class Helper{
+    public function __construct()
+    {
+        echo 'this is magic';
+    }
+}
